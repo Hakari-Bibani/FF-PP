8d60e272bdc3c9b85046a942071c600bb653d2a4
Hakari Bibani <hakary.jalal33@gmail.com>
2024-10-24 14:05:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -464,3 +464,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
